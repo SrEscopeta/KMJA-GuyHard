eff38c970d774cda57c20dfed6ff78b303cae9cd
GoldGlitch <47577403+GoldGlitch@users.noreply.github.com>
2019-06-02 19:50:05 +0200

    commit

---
+++ MJA Studios - Guy Hard sin HDRP/Assets/ScriptsGenerales/Componente.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Componente : MonoBehaviour
+{
+    public GameObject trigger;
+
+    private void OnTriggerExit(Collider other)
+    {
+        trigger.GetComponent<AudioSource>().enabled = false;
+    }
+}
