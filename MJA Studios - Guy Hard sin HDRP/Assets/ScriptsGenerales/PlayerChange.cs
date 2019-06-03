2c3f4df8479146a7e2df1d9b7cdedb8f6541c8cc
GoldGlitch <47577403+GoldGlitch@users.noreply.github.com>
2019-06-03 19:56:41 +0200

    Commit

---
+++ MJA Studios - Guy Hard sin HDRP/Assets/ScriptsGenerales/PlayerChange.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerChange : MonoBehaviour
+{
+    public GameObject player2;
+    public GameObject player1;
+    public GameObject Camera;
+    public GameObject text;
+
+    public bool camera1;
+    public bool activePlayer2;
+    public bool activePlayer1 = true;
+
+    public string tpPosition;
+
+    private void Start()
+    {
+        player2.SetActive(activePlayer2);
+        player1.SetActive(activePlayer1);
+
+        Camera.SetActive(camera1);
+        text = GameObject.Find("Change");
+    }
+
+    int count = 0;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            text.gameObject.GetComponent<Text>().enabled = true;
+            if (Input.GetKeyDown(KeyCode.E) && count == 0)
+            {
+                activePlayer2 = !activePlayer2;
+                activePlayer1 = !activePlayer1;
+                camera1 = !camera1;
+
+                player2.SetActive(activePlayer2);
+                player1.SetActive(activePlayer1);
+                Camera.SetActive(camera1);
+
+
+                player2.gameObject.GetComponent<NoDestroy>().tpPosition = tpPosition;
+                player1.gameObject.GetComponent<NoDestroy>().tpPosition = tpPosition;
+                player1.gameObject.GetComponent<NoDestroy>().tpStart = true;
+                player2.gameObject.GetComponent<NoDestroy>().tpStart = true;
+            }
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        text.gameObject.GetComponent<Text>().enabled = false;
+    }
+
+}
