b25111d058a39792223d6e1257f5fa57251329c4
GoldGlitch <47577403+GoldGlitch@users.noreply.github.com>
2019-06-04 15:00:53 +0200

    Dios no existe

---
+++ MJA Studios - Guy Hard sin HDRP/Assets/ScriptsGenerales/Count.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Count : MonoBehaviour
+{
+    int countCasa = 0;
+    int countPuerto = 0;
+    int countComisaria = 0;
+    int countMcRonald = 0;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "casa")
+        {
+            countCasa =+ 1;
+        }
+
+        if(other.gameObject.tag == "Puerto")
+        {
+            countPuerto = +1;
+        }
+
+        if (other.gameObject.tag == "Comisaria")
+        {
+            countComisaria = +1;
+        }
+
+        if (other.gameObject.tag == "McRonald")
+        {
+            countMcRonald = +1;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+}
