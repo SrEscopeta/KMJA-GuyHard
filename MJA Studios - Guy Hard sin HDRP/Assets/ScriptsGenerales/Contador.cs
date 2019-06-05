d456d200760f00191ac72c0793cc6c99b8981d20
GoldGlitch <47577403+GoldGlitch@users.noreply.github.com>
2019-06-05 05:20:19 +0200

    war of pushes

---
+++ MJA Studios - Guy Hard sin HDRP/Assets/ScriptsGenerales/Contador.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contador : MonoBehaviour
+{
+    public int countCasa;
+    public int countPuerto;
+    public int countComisaria;
+    public int countMcRonald;
+    public GameObject casaAsquerosa;
+
+    private void Start()
+    {
+        casaAsquerosa = GameObject.FindGameObjectWithTag("Casa");
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Casa")
+        {
+            countCasa ++;
+            Debug.Log("valor: " + countCasa);
+        }
+
+        if (other.gameObject.tag == "Puerto")
+        {
+           
+        }
+
+        if (other.gameObject.tag == "Comisaria")
+        {
+            
+        }
+
+        if (other.gameObject.tag == "McRonald")
+        {
+            
+        }
+    }
+
+    private void Update()
+    {
+        if(countCasa >= 2)
+        {
+            DestroyImmediate(casaAsquerosa);
+        }
+    }
+
+
+}
